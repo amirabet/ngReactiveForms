8f0ac05dab8aaf31cfda1317b8c8616b68da08fc
Artur Mirabet <arturmirabet@gmail.com>
2024-04-15 11:49:32 +0200

    Basic and dynamic form validations

--- formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTC 5090',
+  price: 4100,
+  inStorage: 20
+} 
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+
+export class BasicPageComponent implements OnInit{
 
   /*
   * Para crear el Formulario reactivo
@@ -25,13 +32,83 @@ export class BasicPageComponent {
   public myForm: FormGroup = this.formBuilder.group({
   //  propName: [ 'valueDelCampo', [ValidacionesSíncronas], [ValidacionesAsíncronas] ]
     name: ['', [ Validators.required, Validators.minLength(3) ]],
-    price: [0],
-    inStorage: [0],
+    price: [0, [ Validators.required, Validators.min(0) ]],
+    inStorage: [0, [ Validators.required, Validators.min(0) ]],
   });
 
+  /*
+  * El escenario más comun es el de recibir de back
+  * los datos iniciales y rellenar el form en el OnInit
+  */
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
+  }
+
+  /*
+  * Podemos usar un método / getter para obtener lo mensajes de validación
+  */
+ isInvalidField (field: string): boolean | null {
+  return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+ }
+
+ getFieldError (field: string): string | null {
+    if( !this.myForm.controls[field] && !this.myForm.controls[field].errors )
+        return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+    console.log({errors})
+
+    for (const key of Object.keys(errors)) {
+      switch( key ){
+        case 'required':
+          return 'Este campo es requerido';
+
+          case 'minlength':
+            return `La longitud mínima es ${ errors['minlength'].requiredLength } caracter/es`;
+      }
+    }
+
+    return null;
+ }
 
   onSave():void {
-    console.log( this.myForm.value);
+    if( this.myForm.invalid) {
+      this.myForm.markAllAsTouched(); // Dispara validaciones en todos los campos si trabajamos con Touched
+      return;
+    }
+
+    console.log( this.myForm.value );
+
+    /*
+    * Volvemos a recuperar los valores
+    * y la hacer reset las propiedades 
+    * Pristine y Touched del formulario
+    * también se resetean
+    * 
+    * Podríamos usar una función como la que se
+    * emplea en el OnInit
+    */
+    this.myForm.reset(
+      {
+        name: '',
+        price: 41,
+        inStorage: 0
+      }
+    )
+
+    /*
+    * Si en cambio usamos el setValue
+    * también cambiamos el valor de los campos
+    * per Pristine y Touched mantienen su valor
+    */
+    // this.myForm.setValue(
+    //   {
+    //     name: '',
+    //     price: 41,
+    //     inStorage: 0
+    //   }
+    // )
   }
 
 }
--- formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -6,4 +7,95 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    /* Esta propiedad es un listado de juegos
+    * favoriteGames: new FormArray([]) => en vez de new FormControl()
+    */
+    favoriteGames: this.formBuilder.array([
+      ['Metal Gear', Validators.required],
+      ['Castlevania', Validators.required],
+    ])
+  })
+
+  public newFavorite: FormControl = new FormControl('', Validators.required );
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  /*
+  * Getter de Favorite Games 
+  * observar el cast "as FormArray"
+  */
+  get favoriteGamesControl() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  isInvalidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field] && !this.myForm.controls[field].errors)
+      return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `La longitud mínima es ${errors['minlength'].requiredLength} caracter/es`;
+      }
+    }
+
+    return null;
+  }
+
+  isInvalidFieldInArray(formArray: FormArray, index: number) {
+    return formArray.controls[index].errors
+      && formArray.controls[index].touched;
+  }
+
+  onDeleteFavorite(index: number): void {
+    this.favoriteGamesControl.removeAt(index);
+  }
+
+  onAddFavorite(): void {
+    if (this.newFavorite.invalid)
+      return;
+
+    const newFavorite: string = this.newFavorite.value;
+
+    // Agregar al array generando un new FormControl
+    //this.favoriteGamesControl.push( new FormControl( newFavorite, Validators.required ) );
+
+    // Agregar usando el formBuilder (recomendado)
+    this.favoriteGamesControl.push(
+      this.formBuilder.control( newFavorite, Validators.required )
+    );
+
+    // Reseteamos el campo
+    this.newFavorite.reset();
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    /*
+    * Reseteamos el valor de favoriteGames igualándolo
+    * a un nuevo array de formBuilder
+    * OJO el casteo "as FormArray"
+    */
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.formBuilder.array([]);
+
+    // Borramos el resto de valores
+    this.myForm.reset();
+  }
 }
